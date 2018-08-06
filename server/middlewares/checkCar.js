9b097f70c09cc15e9e4ee9dc140efa4fe1b843e9
AliA1997-Medium <devmtnali@gmail.com>
2018-08-06 15:11:17 -0700

    Completed Middlewares

--- server/middlewares/checkCar.js
+++ server/middlewares/checkCar.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res, next) {
     if(!req.session.user) {
-        res.status(404).json({message: 'Must be logged in order to create a car!'});
+        res.json({message: 'Must be logged in order to create a car!'}).end();
     }
     next();
 }
